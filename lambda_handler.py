1e561f1778c70fea7698652c827c6ec7eb63f39f
levi <m.r.pahlevi00@gmail.com>
2020-10-09 13:57:41 +0700

    fix lambda_handler

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,9 +1,8 @@
 import json
 
 def lambda_handler(event, context):
-    
-	print(json.dumps(event))
-	return {
-		'statusCode': 200,
-		'body': json.dumps('logs printed!')
-	}
+    print(json.dumps(event))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('logs printed!')
+    }
